75c0cc9d3f39bf73d907400cdc0d0144dd022309
Enver Ekrem Uçar <ekremucar@gmail.com>
2020-12-05 15:04:32 +0300

    versions

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import os
 import numpy as np
 import tensorflow as tf
-from keras.applications.vgg16 import preprocess_input, decode_predictions
-from keras.models import load_model
-from keras.preprocessing.image import img_to_array, load_img
+
+from tensorflow.keras.applications.vgg16 import preprocess_input, decode_predictions
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
 from flask import Flask, redirect, url_for, request, render_template
 
 # define a Flask app
